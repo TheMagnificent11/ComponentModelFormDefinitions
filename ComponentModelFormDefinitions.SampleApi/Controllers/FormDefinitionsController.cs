6ac471743427f41dd1575f39d20fd247c53c3c37
Saji Weerasingham <saji.weerasingham@outlook.com>
2018-10-28 06:15:17 +1000

    added sample api website

---
+++ ComponentModelFormDefinitions.SampleApi/Controllers/FormDefinitionsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using ComponentModelFormDefinitions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    /// <summary>
+    /// Form Defintions Controller
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    [AllowAnonymous]
+    public sealed class FormDefinitionsController : Controller
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FormDefinitionsController"/> class
+        /// </summary>
+        /// <param name="manager">Form definition manager</param>
+        public FormDefinitionsController(FormDefinitionManager manager)
+        {
+            Manager = manager;
+        }
+
+        private FormDefinitionManager Manager { get; }
+
+        /// <summary>
+        /// Gets all the form defintions
+        /// </summary>
+        /// <returns>
+        /// HTTP 200 containing an enumerable list of form definitions
+        /// HTTP 404 if not form defintions exist
+        /// </returns>
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<FormDefinition>))]
+        public ActionResult<IEnumerable<FormDefinition>> GetAll()
+        {
+            var webModelsAssembly = GetWebModelsAssembly();
+            if (webModelsAssembly == null) return NotFound();
+
+            var formDefintions = new List<FormDefinition>();
+            var requestTypes = GetRequestTypes(webModelsAssembly);
+
+            requestTypes
+                .ToList()
+                .ForEach(i => formDefintions.Add(Manager.GetFormDefinition(i)));
+
+            return Ok(formDefintions);
+        }
+
+        /// <summary>
+        /// Gets a form definition for the specified ID
+        /// </summary>
+        /// <param name="formId">ID of form defnition to retrieve</param>
+        /// <returns>
+        /// HTTP 200 containing the form defintion if it exists
+        /// HTTP 404 if the a form definition with the specified ID does not exist
+        /// </returns>
+        [HttpGet]
+        [Route("{formId}")]
+        [ProducesResponseType(200, Type = typeof(FormDefinition))]
+        [ProducesResponseType(404)]
+        public ActionResult<FormDefinition> Get(string formId)
+        {
+            var webModelsAssembly = GetWebModelsAssembly();
+            if (webModelsAssembly == null) return NotFound();
+
+            var requestType = GetRequestType(webModelsAssembly, formId);
+            if (requestType == null) return NotFound();
+
+            var formDefintion = Manager.GetFormDefinition(requestType);
+            if (formDefintion == null) return NotFound();
+
+            return Ok(formDefintion);
+        }
+
+        private static Assembly GetWebModelsAssembly()
+        {
+            return Assembly.Load("Api.Models");
+        }
+
+        private static IEnumerable<Type> GetRequestTypes(Assembly assembly)
+        {
+            foreach (Type type in assembly.GetTypes())
+            {
+                if (type.GetCustomAttributes(typeof(RequestModelAttribute), true).Length > 0)
+                {
+                    yield return type;
+                }
+            }
+        }
+
+        private static Type GetRequestType(Assembly assembly, string formId)
+        {
+            foreach (Type type in assembly.GetTypes())
+            {
+                var requestModelAttributes = type.GetCustomAttributes(typeof(RequestModelAttribute), true);
+                if (requestModelAttributes == null || requestModelAttributes.Length == 0) continue;
+
+                var attribute = requestModelAttributes[0] as RequestModelAttribute;
+                if (attribute.FormId == formId) return type;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ComponentModelFormDefinitions.SampleApi/Startup.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace ComponentModelFormDefinitions.SampleApi
+{
+    public class Startup
+    {
+        private const string ApiName = "Sample API";
+        private const string CorsPlolicyName = "CorsPolicy";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public static void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseCors(CorsPlolicyName);
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", ApiName);
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            ConfigureCors(services);
+            ConfigureSwagger(services);
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddTransient<FormDefinitionManager>();
+        }
+
+        private static void ConfigureCors(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(
+                    CorsPlolicyName,
+                    policy =>
+                        policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+        }
+
+        private static void ConfigureSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = ApiName, Version = "v1" });
+            });
+        }
+    }
+}
