3a121322a2631891dd83c93ac5efcda69ede5e9e
Saji Weerasingham <saji.weerasingham@outlook.com>
2018-10-28 06:15:59 +1000

    removed comments

--- ComponentModelFormDefinitions.SampleApi/Controllers/FormDefinitionsController.cs
+++ ComponentModelFormDefinitions.SampleApi/Controllers/FormDefinitionsController.cs
@@ -8,18 +8,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
 {
-    /// <summary>
-    /// Form Defintions Controller
-    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     [AllowAnonymous]
     public sealed class FormDefinitionsController : Controller
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="FormDefinitionsController"/> class
-        /// </summary>
-        /// <param name="manager">Form definition manager</param>
         public FormDefinitionsController(FormDefinitionManager manager)
         {
             Manager = manager;
@@ -27,13 +20,6 @@ namespace Api.Controllers
 
         private FormDefinitionManager Manager { get; }
 
-        /// <summary>
-        /// Gets all the form defintions
-        /// </summary>
-        /// <returns>
-        /// HTTP 200 containing an enumerable list of form definitions
-        /// HTTP 404 if not form defintions exist
-        /// </returns>
         [HttpGet]
         [ProducesResponseType(200, Type = typeof(IEnumerable<FormDefinition>))]
         public ActionResult<IEnumerable<FormDefinition>> GetAll()
@@ -51,14 +37,6 @@ namespace Api.Controllers
             return Ok(formDefintions);
         }
 
-        /// <summary>
-        /// Gets a form definition for the specified ID
-        /// </summary>
-        /// <param name="formId">ID of form defnition to retrieve</param>
-        /// <returns>
-        /// HTTP 200 containing the form defintion if it exists
-        /// HTTP 404 if the a form definition with the specified ID does not exist
-        /// </returns>
         [HttpGet]
         [Route("{formId}")]
         [ProducesResponseType(200, Type = typeof(FormDefinition))]
