6a312c9744c9935a6f646501263f54917f83f356
Saji Weerasingham <saji.weerasingham@outlook.com>
2018-10-29 07:56:47 +1000

    submit form

--- NgrxComponentModelFormDefinitions/src/app/register/register.component.ts
+++ NgrxComponentModelFormDefinitions/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
 import { FormComponent } from 'ngrx-componentmodelformdefinitions-material';
 import { RegistrationRequest } from './registration-request';
+import { ApiService } from 'ngx-netcore-api';
 
 @Component({
     selector: 'app-register',
@@ -11,14 +13,16 @@ export class RegisterComponent implements OnInit {
 
     @ViewChild('form') form: FormComponent<RegistrationRequest>;
 
-    constructor() { }
+    constructor(private api: ApiService) { }
 
     ngOnInit() {
     }
 
     onSubmit(request: RegistrationRequest): void {
-        if (!request) return;
-        alert(request.email);
+        if (!request || !request.email) return;
+
+        this.api.post('users', request)
+            .subscribe(() => alert('yay'), () => alert('boo'));
     }
 
 }
