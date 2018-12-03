bf9a8fb3570f7110b7cf85bb24bfc53c3751fede
Saji Weerasingham <saji.weerasingham@outlook.com>
2018-12-04 08:05:20 +1000

    added observable input into form component to read existing data

--- NgrxComponentModelFormDefinitions/projects/ngrx-componentmodelformdefinitions-material/src/lib/form/form.component.ts
+++ NgrxComponentModelFormDefinitions/projects/ngrx-componentmodelformdefinitions-material/src/lib/form/form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { Store } from '@ngrx/store';
+import { Observable, combineLatest, of } from 'rxjs';
 import { takeWhile, filter, } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
 import { DynamicFormControlModel, DynamicFormService, DynamicInputModel } from '@ng-dynamic-forms/core';
 import { FormsState, GetFormDefinitionAction, FormDefinition, formDefinition } from 'ngrx-componentmodelformdefinitions';
 import { FormDefinitionConverter } from '../form-definition-converter';
@@ -15,6 +16,7 @@ export class FormComponent<T> implements OnInit, OnDestroy {
 
     @Input('form-id') formId: string;
     @Input('submit-button-label') submitButtonLabel = 'Submit';
+    @Input('get-entity') getEntity$: Observable<T>;
     @Output() submit = new EventEmitter<T>();
 
     isLoaded = false;
@@ -23,22 +25,30 @@ export class FormComponent<T> implements OnInit, OnDestroy {
 
     private alive = true;
     private formDef: FormDefinition;
+    private subscriptions$: Observable<[FormDefinition, T]>;
 
     constructor(
         public formService: DynamicFormService,
         private store: Store<FormsState>
-    ) { }
+    ) {
+        const formDef$ = this.store.select(formDefinition);
+
+        if (this.getEntity$) {
+            this.subscriptions$ = combineLatest(formDef$, this.getEntity$);
+        } else {
+            this.subscriptions$ = combineLatest(formDef$, of({} as T));
+        }
+    }
 
     ngOnInit() {
-        this.store
-            .select(formDefinition)
-            .pipe(
-                takeWhile(() => this.alive),
-                filter(form => !!form)
-            )
-            .subscribe(resp => {
-                this.formDef = resp;
-                this.formModel = FormDefinitionConverter.convert(resp);
+        this.subscriptions$
+            .pipe(takeWhile(() => this.alive))
+            .subscribe(([formDef, model]) => {
+                if (!formDef) return;
+
+                this.formDef = formDef;
+                this.formModel = FormDefinitionConverter.convert(formDef);
+                this.setFormValues(model);
                 this.formGroup = this.formService.createFormGroup(this.formModel);
                 this.isLoaded = true;
             });
@@ -67,6 +77,19 @@ export class FormComponent<T> implements OnInit, OnDestroy {
         if (!formField) return null;
 
         return formField.value;
+    }
+
+    private setFormValues(model: T): void {
+        for (const item of this.formModel) {
+            const value = model[item.id];
+            if (!value) continue;
+
+            if (!(item instanceof DynamicInputModel)) continue;
+
+            const input = item as DynamicInputModel;
+
+            input.valueUpdates.next(value);
+        }
     }
 
 }
