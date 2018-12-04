fcb0d9afc9d635a0a40f0e98f3ca4360ad07b2ef
Saji Weerasingham <saji.weerasingham@outlook.com>
2018-12-04 17:31:11 +1000

    started implementing get user api route

---
+++ ComponentModelFormDefinitions.SampleApi/Domain/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ComponentModelFormDefinitions.SampleApi.Domain
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string GivenName { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+    }
+}
